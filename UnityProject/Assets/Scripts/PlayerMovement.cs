cabde0acb2948dd5e740f6a07f437d20b2445687
Noitiro <157133387+Noitiro@users.noreply.github.com>
2025-11-11 22:01:10 +0100

    Dodanie komentarza do PlayeMovment

--- UnityProject/Assets/Scripts/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("PlayerMovement script has started.");
+        //Polskaaaaa!
     }
 
     // Update is called once per frame
