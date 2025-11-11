b4738c5b7046cb8dc67feb7555a28bcf13992b71
Kosek7 <dawid@arigato.pl>
2025-11-11 21:58:36 +0100

    dodanie print do poruszania postaci

--- UnityProject/Assets/Scripts/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,11 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+<<<<<<< Updated upstream
         // XD tololololo
+=======
+        print("PlayerMovement script has started.");
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
