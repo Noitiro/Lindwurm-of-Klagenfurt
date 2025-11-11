c5066497b90e7e0e7cc7734be1bb799adc4d0c4d
Noitiro <157133387+Noitiro@users.noreply.github.com>
2025-11-11 23:54:24 +0100

    Movement test 0.0.0.0.1

---
+++ UnityProject/Assets/Scripts/GG.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class GG : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+        Debug.Log(collision.name + "Gratulujemy wygranej!!!");
+    }
+}
--- UnityProject/Assets/Scripts/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //Polskaaaaa!
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField] private float speed = 0.5f;
+    private Rigidbody2D rb;
+    private Vector2 input;
+
+    InputSystem_Actions inputSystemActions;
+
+    private void Awake() {
+        inputSystemActions = new InputSystem_Actions();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnEnable() {
+        inputSystemActions.Enable();
+    }
+
+    private void OnDisable() {
+        inputSystemActions.Disable(); 
+    }
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        input = inputSystemActions.Player.Move.ReadValue<Vector2>();
+        input.Normalize();
+        if(input.x < 0) {
+            rb.transform.localScale = new Vector3(5,5,5);
+        }else if(input.x > 0) {
+            rb.transform.localScale = new Vector3(-5, 5, 5);
+        }
+    }
+
+    void FixedUpdate() {
+        rb.linearVelocity = input * speed;
     }
 }
