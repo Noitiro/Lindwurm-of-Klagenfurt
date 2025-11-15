b653bc17ef16dd154d9ad104d8c009ab72a0fff6
Gabriel <gabriel.biskup08@gmail.com>
2025-11-15 11:10:08 +0100

    Added a follow script

--- UnityProject/Assets/Scripts/EnemiesFollows.cs
+++ UnityProject/Assets/Scripts/EnemiesFollows.cs
@@ -1,21 +1,34 @@
 using UnityEngine;
 
 public class EnemiesFollows : MonoBehaviour {
-    
-    [SerializeField] private float speed = 2f;
-    Rigidbody2D rb;
-    Transform target;
-    Vector2 moveDirection;
 
-    public void Awake() {
+    [SerializeField] private float speed = 2f;
+
+    private Rigidbody2D rb;
+    private Transform target;
+    private Vector2 moveDirection;
+
+    void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
+
     void Start() {
-        
+        GameObject player = GameObject.Find("Player");
+        if (player != null) {
+            target = player.transform;
+        }
     }
 
     void Update() {
-        
+        if (target != null) {
+            Vector2 direction = (target.position - transform.position).normalized;
+            moveDirection = direction;
+        }
     }
-    
+
+    void FixedUpdate() {
+        if (target != null) {
+            rb.linearVelocity = moveDirection * speed;
+        }
+    }
 }
