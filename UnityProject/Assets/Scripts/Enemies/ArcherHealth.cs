9ad6e4df8e0edf7007e076535a6309eee3325e95
Halcio65 <michal.ciok.33@gmail.com>
2025-11-23 12:42:53 +0100

    baseenemyhealth i healthbary na wszytkich prefabach

--- UnityProject/Assets/Scripts/Enemies/ArcherHealth.cs
+++ UnityProject/Assets/Scripts/Enemies/ArcherHealth.cs
@@ -1,25 +1,5 @@
 using UnityEngine;
 
-public class ArcherHealth : MonoBehaviour, IDamageable {
-    private float maxHealth = 100;
-    private float currentHealth;
+public class ArcherHealth : BaseEnemyHealth {
 
-    private void Awake() {
-        currentHealth = maxHealth;
-    }
-
-    public void Damage(float amount) {
-        currentHealth -= amount;
-
-        Debug.Log("Enemy takes damage: " + amount);
-
-        if (currentHealth <= 0) {
-            Die();
-        }
-    }
-
-    private void Die() {
-        Debug.Log("Przeciwnik zginął!");
-        Destroy(gameObject);
-    }
 }
--- UnityProject/Assets/Scripts/Enemies/EnemyHealthBar.cs
+++ UnityProject/Assets/Scripts/Enemies/EnemyHealthBar.cs
@@ -1,27 +1,30 @@
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
+
 public class EnemyHealthBar : MonoBehaviour {
     [SerializeField] private Image healthBarImage;
 
-    [SerializeField] private EnemiesHealth enemyHealth;
+    [SerializeField] private BaseEnemyHealth enemyHealth;
 
     void Start() {
 
         if (enemyHealth == null) {
-            enemyHealth = GetComponentInParent<EnemiesHealth>();
+            enemyHealth = GetComponentInParent<BaseEnemyHealth>();
         }
     }
+
     void Update() {
         if (enemyHealth != null) {
-
             float fillValue = enemyHealth.CurrentHealth / enemyHealth.MaxHealth;
-
             healthBarImage.fillAmount = fillValue;
+
+            if (enemyHealth.CurrentHealth <= 0) {
+                gameObject.SetActive(false);
+            }
+        }
+        else {
+            Destroy(gameObject);
         }
         transform.rotation = Quaternion.identity;
-
-        if (enemyHealth.CurrentHealth <= 0) {
-            gameObject.SetActive(false);
-        }
     }
 }
