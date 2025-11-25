4a1c40145eff70021f822f0131cb762f205bad8b
Noitiro <157133387+Noitiro@users.noreply.github.com>
2025-11-25 22:45:54 +0100

    new asset

--- UnityProject/Assets/Scripts/Mechanic/DestroyHouses.cs
+++ UnityProject/Assets/Scripts/Mechanic/DestroyHouses.cs
@@ -4,13 +4,17 @@ public class DestroyHouses : MonoBehaviour, IDamageable {
     [SerializeField] Sprite destroyHouseSprite;
     [SerializeField] bool destroy = false;
     private SpriteRenderer spriteRenderer;
+    private Animator anim;
+
 
     private void Awake() {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
     }
 
     public void DestroyHouse() {
-            spriteRenderer.sprite = destroyHouseSprite;
+        anim.SetTrigger("destroy");
     }
 
     public void Damage(float amount) {
