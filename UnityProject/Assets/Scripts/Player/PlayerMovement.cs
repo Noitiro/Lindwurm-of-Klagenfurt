7a5f70a5285d219b4859ef5948061f20af212be5
Noitiro <157133387+Noitiro@users.noreply.github.com>
2025-11-29 23:17:01 +0100

    stamina work and animation

--- UnityProject/Assets/Scripts/Player/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,10 +1,22 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using UnityEngine.Windows;
 
 public class PlayerMovement : MonoBehaviour {
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float normalSpeed = 2f;
+    [SerializeField] private float sprintSpeed = 3f;
+    [SerializeField] Image staminaBar;
+    [SerializeField] GameObject parentHitbox;
+
+    private float moveSpeed;
+    private bool canSprint;
+    private bool pressSprint;
+    private float maxEnergy = 100f;
+    private float currentEnergy = 100f;
+    private float sprintCost = 20f;
+    private float regenRate = 10f;
 
     private Vector2 moveInput;
     private Rigidbody2D rb;
@@ -17,6 +29,7 @@ public class PlayerMovement : MonoBehaviour {
     void Start() {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        moveSpeed = normalSpeed;
     }
     private void OnEnable() {
         playerController.Enable();
@@ -28,6 +41,7 @@ public class PlayerMovement : MonoBehaviour {
 
     private void Update() {
         Move();
+        Sprint();
         rb.linearVelocity = moveInput * moveSpeed;
     }
 
@@ -47,13 +61,51 @@ public class PlayerMovement : MonoBehaviour {
         moveInput = playerController.Player.Move.ReadValue<Vector2>();
         moveInput.Normalize();
 
+        if(moveInput.x > 0) {
+            parentHitbox.transform.localScale = new Vector3(-1, 1, 1);
+        }else if(moveInput.x < 0) {
+            parentHitbox.transform.localScale = new Vector3(1, 1, 1);
+        }
+
         anim.SetFloat("InputX", moveInput.x);
         anim.SetFloat("InputY", moveInput.y);
     }
 
-    public void UpgradeSpeed(float percent) {
-        moveSpeed *= (1f + percent);
-        moveSpeed *= (1f + percent);
+    private void Sprint() {
+        playerController.Player.Sprint.performed += context => {
+            pressSprint = true;
+        };
+
+        playerController.Player.Sprint.canceled += context => {
+            anim.SetBool("isSprint", false);
+            pressSprint = false;
+        };
+
+        if (anim.GetBool("isWalk") && pressSprint && canSprint && currentEnergy > 0f) {
+            moveSpeed = sprintSpeed;
+            anim.SetBool("isSprint", true);
+
+            currentEnergy = Mathf.Clamp(currentEnergy - sprintCost * Time.deltaTime, 0, maxEnergy);
+            if (currentEnergy == 0f) {
+                canSprint = false;
+                anim.SetBool("isSprint", false);
+                moveSpeed = normalSpeed;
+            }
+        } else {
+            currentEnergy = Mathf.Clamp(currentEnergy + regenRate * Time.deltaTime, 0, maxEnergy);
+            if (currentEnergy > 10f) {
+                canSprint = true;
+            }
+        }
+
+        staminaBar.fillAmount = currentEnergy / 100f;
+
+    }
+
+    public void UpgradeSpeed(float percentNormalSpeed, float percentSprintSpeed) {
+        moveSpeed *= (1f + percentNormalSpeed);
+        normalSpeed *= (1f + percentNormalSpeed);
+        sprintSpeed *= (1f + percentSprintSpeed);
 
         Debug.Log("Szybkoœæ gracza zwiêkszona!");
     }
